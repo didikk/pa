5f43d91f0b8d640ca50ad6cec5a67c66674b2ca1
didikk <ismawantodidik@gmail.com>
2015-05-25 09:20:34 +0700

    commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # pa
+
+This is my final project. I use MeteorJS to read data Atmoshperic Pressure from Agriculture Board. Packages that i use are:
+
+1. donaldaverill:serialport
+2. arunoda:streams
+3. maazalik:highcharts
--- pa.js
+++ pa.js
@@ -16,10 +16,101 @@ if (Meteor.isClient) {
       });
   });*/
   
+  var data;
+  
   dataStream.on('pa', function (pa){
-      var data = pa;
-      $('#data').prepend('<div>' + data + ' KPa' +'</div>');
+      data = pa;
+      $('#data').replaceWith('<h4 id="data" class="center-align">' + data + ' KPa' + '</h4>');
   });
+
+  function builtChart() {
+
+      $(document).ready(function () {
+            Highcharts.setOptions({
+                global: {
+                    useUTC: false
+                }
+            });
+
+            $('#adminChartLevel').highcharts({
+                chart: {
+                    type: 'spline',
+                    animation: Highcharts.svg, // don't animate in old IE
+                    marginRight: 10,
+                    events: {
+                        load: function () {
+
+                            // set up the updating of the chart each second
+                            var series = this.series[0];
+
+                            setInterval(function () {
+                                var x = (new Date()).getTime(), // current time
+                                    y = data;
+                                series.addPoint([x, y], true, true);
+                            }, 2000);
+                        }
+                    }
+                },
+                title: {
+                    text: 'Live data'
+                },
+                xAxis: {
+                    type: 'datetime',
+                    tickPixelInterval: 150
+                },
+                yAxis: {
+                    title: {
+                        text: 'Value'
+                    },
+                    plotLines: [{
+                        value: 0,
+                        width: 1,
+                        color: '#808080'
+                    }]
+                },
+                tooltip: {
+                    formatter: function () {
+                        return '<b>' + this.series.name + '</b><br/>' +
+                            Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
+                            Highcharts.numberFormat(this.y, 2);
+                    }
+                },
+                legend: {
+                    enabled: false
+                },
+                exporting: {
+                    enabled: false
+                },
+                series: [{
+                    name: 'Pressure data',
+                    data: (function () {
+                        // generate an array of random data
+                        var data = [],
+                            time = (new Date()).getTime(),
+                            i;
+
+                        for (i = -19; i <= 0; i += 1) {
+                            data.push({
+                                x: time + i * 2000,
+                                y: Math.random() * (111 - 100) + 100
+                            });
+                        }
+                        return data;
+                    }())
+                }]
+            });
+        });
+  };
+
+  Template.adminChartLevel.helpers({
+   
+  });
+
+  Template.adminChartLevel.rendered = function() {    
+      this.autorun(function (c) {
+          builtChart();
+      });
+  };
   
 };
 
@@ -34,7 +125,6 @@ if (Meteor.isServer) {
   });
 
   serialPort.on('data', function(data) {
-    //console.log('message ' + data);
     var str = data.split("#");
     var nilai = str[5].split(":");
     var pa = parseFloat(nilai[1]);
@@ -44,10 +134,5 @@ if (Meteor.isServer) {
       dataStream.emit('pa', pa);
       //console.log('message', isNaN(pa));
     }
-    
-
-    /*Pressure.insert({
-      data: data
-    });*/
   });
 };
